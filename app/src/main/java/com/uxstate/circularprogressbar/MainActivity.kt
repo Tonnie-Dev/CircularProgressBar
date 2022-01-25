25e7b84ce9be755228fa0643977c11ec2618fdb2
Antony <vontonnie@gmail.com>
2022-01-25 08:55:13 +0300

    Add Canvas

--- app/src/main/java/com/uxstate/circularprogressbar/MainActivity.kt
+++ app/src/main/java/com/uxstate/circularprogressbar/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -44,7 +47,7 @@ fun CircularProgressBar(
     var animationPlayed by remember { mutableStateOf(false) }
 
 
-    val currentPercentage = animateFloatAsState(
+    val currentPercentage by animateFloatAsState(
         targetValue = if (animationPlayed) percentage else 0f,
         animationSpec = tween(
             durationMillis = animDuration,
@@ -55,10 +58,27 @@ fun CircularProgressBar(
 
     //add launchEffect
 
-    LaunchedEffect(key1 = true, block = { animationPlayed = true})
+    LaunchedEffect(key1 = true, block = { animationPlayed = true })
 
 
     //Box container with canvas
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.size(radius * 2)){}
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.size(radius * 2)) {
+
+        //on canvas we can draw anything we want
+        Canvas(modifier = Modifier.size(radius * 2), onDraw = {
+
+
+            //inside the DrawScope we have multiple methods e.g. drawArc
+
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercentage,
+                useCenter = false,
+                style = Stroke(width = strokeWidth.toPx(),  cap = StrokeCap.Round)
+            )
+
+        })
+    }
 
 }
