fcab3625808871b5663b43a16348b46df193c0ee
Antony <vontonnie@gmail.com>
2022-01-25 08:15:43 +0300

    Create CircularProgressBar composable

--- app/src/main/java/com/uxstate/circularprogressbar/MainActivity.kt
+++ app/src/main/java/com/uxstate/circularprogressbar/MainActivity.kt
@@ -3,11 +3,12 @@ package com.uxstate.circularprogressbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.uxstate.circularprogressbar.ui.theme.CircularProgressBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,3 +22,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+
+}
