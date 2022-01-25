4429a8bc834223e24ca454636b067fbc2087f608
Antony <vontonnie@gmail.com>
2022-01-25 09:00:33 +0300

    Add Text in the Middle

--- app/src/main/java/com/uxstate/circularprogressbar/MainActivity.kt
+++ app/src/main/java/com/uxstate/circularprogressbar/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -75,10 +77,20 @@ fun CircularProgressBar(
                 startAngle = -90f,
                 sweepAngle = 360 * currentPercentage,
                 useCenter = false,
-                style = Stroke(width = strokeWidth.toPx(),  cap = StrokeCap.Round)
+                style = Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Round)
             )
 
         })
+
+        //text in the middle
+
+        Text(
+            text = (currentPercentage * number).toInt() //change to int to get whole number
+                    .toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 
 }
